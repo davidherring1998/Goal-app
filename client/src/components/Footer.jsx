1e5cbd72bb4bf8942303e5166c2226534626cc34
David Herring <davidherring@Davids-MBP.home>
2023-04-12 17:44:17 -0500

    added footer icons and a couple css fixes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,11 +9,29 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-      <span id="footer_id" >&copy; David Herring 2023</span>
+        <span id="footer_id">&copy; David Herring 2023</span>
         <div className="icon-container">
-        <AiFillGithub />
-        <AiFillLinkedin className="mid-icon" />
-        <AiFillTwitterCircle />
+          <a
+            href="https://www.linkedin.com/in/david-herring-051bba241/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillLinkedin />
+          </a>
+          <a
+            href="https://github.com/davidherring1998"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="mid-icon" />
+          </a>
+          <a
+            href="https://twitter.com/DavidHe95802361"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillTwitterCircle />
+          </a>
         </div>
       </footer>
     </>

5c9fed25695d822f034163a059492381b94c76e0
David Herring <davidherring@Davids-MBP.home>
2023-04-13 17:53:45 -0500

    pushing to heroku

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@ app.use("/api/users", userRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) =>
+  app.get('/*', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '../', 'client', 'build', 'index.html')
     )
